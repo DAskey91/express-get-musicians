d9eb6762d6835a1fffd9eec4b949db537842a231
DAskey91 <daniel.askey@barclays.com>
2024-05-21 11:27:43 +0100

    First Task completed

--- index.test.js
+++ index.test.js
@@ -12,10 +12,23 @@ const seedMusician = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    test("Testing musicians endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+    })
     
-    
+    test("Testing music endpoint", async () => {
+        // Sends request to `/musicians` endpoint
+        const response = await request(app).get("/musicians");
+        expect(response.statusCode).toBe(200);
+    })
 
-
+    test("Testing it returns an array", async () => {
+        // Sends request to `/bakedGoods` endpoint
+        const response = await request(app).get("/musicians");
+        const responseData = JSON.parse(response.text);
+        expect(Array.isArray(responseData)).toBe(true)
+    })
 
 
     
