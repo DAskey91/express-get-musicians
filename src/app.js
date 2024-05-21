b6d6b283e592e29e0e326ee72c40e6131f6850f6
DAskey91 <daniel.askey@barclays.com>
2024-05-21 15:23:04 +0100

    Tasks 2 and 3 completed

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 //TODO: Create a GET /musicians route to return all musicians 
 
 app.get("/musicians", async (request, response) => {
@@ -12,8 +14,25 @@ app.get("/musicians", async (request, response) => {
     response.json(musicians)
     })
 
+    app.get("/musicians/:id", async (req, res) => {
+        const musician = await Musician.findByPk(req.params.id)
+        res.json(musician)
+    })
 
-
+    app.post("/musicians", async (req, res) => {
+        const createdMusician = await Musician.create(req.body)
+        res.json(createdMusician)
+    })
+    
+    app.put("/musicians/:id", async (req, res) => {
+        const updatedMusician = await Musician.update(req.body, {where: {id: req.params.id}})
+        res.json(updatedMusician)
+    })
+    
+    app.delete("/musicians/:id", async (req, res) => {
+        const deletedMusician = await Musician.destroy({where: {id: req.params.id}})
+        res.json(deletedMusician)
+    })
 
 
 
